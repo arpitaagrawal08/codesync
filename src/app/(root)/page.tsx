814d44080a64fccce8b572f351581cb399148bd6
arpitaagrawal08 <168635973+arpitaagrawal08@users.noreply.github.com>
2025-07-11 01:39:23 +0530

    Feat: footer and layout

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,17 @@
+
+export default function Home() {
+  return (
+   <>
+   <div className="min-h-screen">
+    <div className="max-w-[1800px] mx-auto p-4">
+     <div>header</div> 
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        preview and editor
+      </div>
+
+
+    </div>
+   </div>
+   </>
+  );
+}
